7a1141aa0866924e03a4505d1056f0a623569bf1
Aurelian Spodarec <aurelians@libero.it>
2017-05-19 21:44:42 +0200

    added some header and css - few bugs need to be fixed

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+
+//Burger Menu Toggle
+var navToggle = document.querySelector(".site-header__hamburger");
+var menuToggle = document.querySelector(".site-nav__navigation");
+
+navToggle.addEventListener('click', function() {
+
+	navToggle.classList.toggle("is-active");
+	menuToggle.classList.toggle("site-nav__navigation");
+
+}, false);
