a61281fafbf53ee109f079f437fb9c250905bc01
Aurelian Spodarec <aurelians@libero.it>
2017-05-21 15:30:57 +0200

    added search toggle on mobile and other header stuff or something

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 
-//Burger Menu Toggle
+// //Burger Menu Toggle
 var navToggle = document.querySelector(".site-header__hamburger");
 var menuToggle = document.querySelector(".site-nav__navigation");
 
@@ -7,5 +7,18 @@ navToggle.addEventListener('click', function() {
 
 	navToggle.classList.toggle("is-active");
 	menuToggle.classList.toggle("is-active");
+
+
+}, false);
+
+
+//Search
+var siteHeaderSearchIcon = document.querySelector(".site-header__search-icon");
+var siteHeaderSearch = document.querySelector(".site-header__search");
+
+siteHeaderSearchIcon.addEventListener('click', function() {
+
+	siteHeaderSearchIcon.classList.toggle("is-active");
+	siteHeaderSearch.classList.toggle("is-active");
 
 }, false);
